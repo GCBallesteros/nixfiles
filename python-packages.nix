24024a8ca92cabd501241a4b29f11c209b29b3ad
Guillem Ballesteros <guillem@air.local>
2022-10-23 18:33:28 +0100

    Formatting

--- python-packages.nix
+++ python-packages.nix
@@ -1,12 +1,13 @@
 { pkgs }:
 
-pkgs.python3.withPackages (p: with p; [
-  ipython # interactive shell
-  jupyter # interactive notebooks
-  matplotlib # plots
-  numpy # numerical computation
-  scipy # numerical computation
-  pandas # data analysis
-  requests # HTTP library
-  setuptools # setup.py
-])
+pkgs.python3.withPackages (p:
+  with p; [
+    ipython # interactive shell
+    jupyter # interactive notebooks
+    matplotlib # plots
+    numpy # numerical computation
+    scipy # numerical computation
+    pandas # data analysis
+    requests # HTTP library
+    setuptools # setup.py
+  ])
