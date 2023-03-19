00935764dbd1ca8944da80b153d20843b9627f93
Guillem Ballesteros <guillem@maxwellrules.com>
2023-03-19 04:08:43 +0000

    Ensure ruff is installed

--- neovim/lua/plugins/configs/mason.lua
+++ neovim/lua/plugins/configs/mason.lua
@@ -3,5 +3,5 @@ require("mason-lspconfig").setup({
   ensure_installed = { "lua_ls", "rust_analyzer", "pyright", "ruff-lsp" },
 })
 require("mason-tool-installer").setup({
-  ensure_installed = { "stylua", "black", "isort", "flake8" },
+  ensure_installed = { "stylua", "black", "isort", "ruff" },
 })
