da9c2f07af0ef2a8eeb56176cb7dd7d1f9060954
Guillem Ballesteros <guillem@maxwellrules.com>
2023-03-19 10:28:33 +0000

    Add jupyterlab

--- python-packages.nix
+++ python-packages.nix
@@ -4,6 +4,7 @@ pkgs.python3.withPackages (p:
   with p; [
     ipython # interactive shell
     jupyter # interactive notebooks
+    jupyterlab # Better interactive notebooks
     matplotlib # plots
     numpy # numerical computation
     scipy # numerical computation
