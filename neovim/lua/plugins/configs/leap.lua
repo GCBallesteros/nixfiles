dcee491aef8b0c77fa958006394837abf31b03c6
Guillem Ballesteros <guillem@maxwellrules.com>
2023-05-21 19:49:47 +0100

    Give labels to flit

--- neovim/lua/plugins/configs/leap.lua
+++ neovim/lua/plugins/configs/leap.lua
@@ -77,7 +77,7 @@ end
 require("flit").setup({
   keys = { f = "f", F = "F", t = "t", T = "T" },
   -- A string like "nv", "nvo", "o", etc.
-  labeled_modes = "v",
+  labeled_modes = "nv",
   multiline = true,
   -- Like `leap`s similar argument (call-specific overrides).
   -- E.g.: opts = { equivalence_classes = {} }
