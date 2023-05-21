33d5da577863316e2406c4e7599642bbdf8fa394
Guillem Ballesteros <guillem@maxwellrules.com>
2023-05-21 20:06:38 +0100

    Don't accept completion on enter

--- neovim/lua/plugins/configs/cmp.lua
+++ neovim/lua/plugins/configs/cmp.lua
@@ -34,7 +34,6 @@ cmp.setup({
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
     ["<C-e>"] = cmp.mapping.close(),
-    --["<CR>"] = cmp.mapping.confirm({
   },
   sources = {
     { name = "nvim_lsp" },
