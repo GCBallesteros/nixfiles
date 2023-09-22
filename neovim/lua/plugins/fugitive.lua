f7376eec01f94c056668f79880b3feed5c44e22f
Guillem Ballesteros <guillem@maxwellrules.com>
2023-09-22 10:06:07 +0200

    More fugitive shortcuts

--- neovim/lua/plugins/fugitive.lua
+++ neovim/lua/plugins/fugitive.lua
@@ -6,6 +6,8 @@ local M = {
     { "<leader>gg", "<cmd>G<cr>", desc = "Git Fugitive" },
     { "<leader>gp", "<cmd>G push<cr>", desc = "Git Push" },
     { "<leader>gb", "<cmd>G blame<cr>", desc = "Git Blame" },
+    { "<leader>gd", "<cmd>Gdiffsplit!<cr>", desc = "Git Diff file" },
+    { "<leader>gl", "<cmd>GcLog<cr>", desc = "Git Log" },
   },
 }
 
