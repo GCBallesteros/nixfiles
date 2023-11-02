c100ccd601cb0aaca003926029569eff49db0170
Guillem Ballesteros <guillem@kuvaspace.com>
2023-11-02 16:14:10 +0100

    feat(nvim): Disable confirm on return

--- neovim/lua/plugins/nvim-cmp.lua
+++ neovim/lua/plugins/nvim-cmp.lua
@@ -1,4 +1,9 @@
 return {
-	"hrsh7th/nvim-cmp",
-	opts = { completion = { completeopt = "menu,menuone,noinsert,noselect" } },
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.completion.completeopt = "menu,menuone,noinsert,noselect"
+    opts.mapping["<CR>"] = cmp.mapping.confirm({ select = false })
+    return opts
+  end,
 }
