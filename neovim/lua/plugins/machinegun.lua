47eff15da45a7b68d3a6e75418d84a7cb711fb9c
Guillem Ballesteros <guillem@kuvaspace.com>
2023-11-02 10:31:51 +0200

    feat: Add remote ds machine

--- neovim/lua/plugins/machinegun.lua
+++ neovim/lua/plugins/machinegun.lua
@@ -4,11 +4,12 @@ return {
   opts = {
     global = "mg",
     default = "work_desktop",
-    machines = { m2 = "86a39a1a", work_desktop = "c4cd42bf" },
+    machines = { m2 = "86a39a1a", work_desktop = "c4cd42bf", ds_machine = "ad5e10eb" },
     settings = {
       ["guillem@m2"] = { colorscheme = "rose-pine" },
       ["root@m2"] = { colorscheme = "catppuccin-latte" },
       ["work_desktop"] = { colorscheme = "tokyonight" },
+      ["ds_machine"] = { colorscheme = "tokyonight" },
     },
   },
 }
