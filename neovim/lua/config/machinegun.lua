33c8eb079263e5bff2ba05774bdc317d38677a3e
GCBallesteros <dev+github@maxwellrules.com>
2023-11-13 13:41:24 +0100

    feat: Make machinegun config

---
+++ neovim/lua/config/machinegun.lua
@@ -0,0 +1,12 @@
+return {
+  global = "mg",
+  default = "work_desktop",
+  machines = { m2 = "86a39a1a", work_desktop = "c4cd42bf", ds_machine = "ad5e10eb" },
+  settings = {
+    ["guillem@m2"] = { colorscheme = "rose-pine" },
+    ["root@m2"] = { colorscheme = "catppuccin-latte" },
+    ["work_desktop"] = { colorscheme = "tokyonight" },
+    ["ds_machine"] = { colorscheme = "rose-pine" },
+    ["root@ds_machine"] = { colorscheme = "miasma" },
+  },
+}
