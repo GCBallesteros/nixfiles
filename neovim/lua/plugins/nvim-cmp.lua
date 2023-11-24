7ded870a6666a7e9f5e442ae629a24e4c48c9b2b
GCBallesteros <dev+github@maxwellrules.com>
2023-11-24 22:52:06 +0200

    feat: Use border on nvim-cmp popups

--- neovim/lua/plugins/nvim-cmp.lua
+++ neovim/lua/plugins/nvim-cmp.lua
@@ -4,6 +4,11 @@ return {
     local cmp = require "cmp"
     opts.completion.completeopt = "menu,menuone,noinsert,noselect"
     opts.mapping["<CR>"] = cmp.mapping.confirm({ select = false })
+    opts.window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    }
+
     return opts
   end,
 }
