3e74b947328f229690542a109404b65d910ed636
GCBallesteros <dev+github@maxwellrules.com>
2023-12-12 08:00:06 +0100

    feat: set opt.list to false

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -6,3 +6,4 @@ opt.colorcolumn = { 87 } -- columns to highlight
 opt.scrolloff = 5 -- space to leave on the bottom when scrolling down
 opt.cursorcolumn = true
 opt.pumblend = 0  -- no transparency in pop up window
+opt.list = false
