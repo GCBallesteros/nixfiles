838cfc489b111f1a7d769f6a06d8fc73cf775f4e
GCBallesteros <dev+github@maxwellrules.com>
2023-12-14 10:56:50 +0100

    feat: Change per computer config

--- neovim/lua/config/machinegun.lua
+++ neovim/lua/config/machinegun.lua
@@ -4,12 +4,12 @@ return {
   machines = { m2 = "86a39a1a", work_desktop = "c4cd42bf", ds_machine = "ad5e10eb" },
   settings = {
     ["guillem@m2"] = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "tokyonight",
       whaler = {
         directories = { { path = "/Users/guillem/projects", alias = "plugins" } },
         oneoff_directories = { { path = "/Users/guillem/Documents/Projects/nanochess", alias = "Random project" } },
       },
-      dev = { neogit = true, notebooknavigator = true },
+      dev = { neogit = false, notebooknavigator = true },
     },
     ["root@m2"] = { colorscheme = "catppuccin-latte" },
     ["work_desktop"] = { colorscheme = "tokyonight" },
